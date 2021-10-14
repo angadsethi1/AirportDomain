09c2a4e6732bb0544fc0d5e599e86f4f61dc482a
Angad Singh Sethi <AngadSethi@MacBook.local>
2021-10-14 14:42:56 +0100

    commit 1

---
+++ crew.js
@@ -0,0 +1,10 @@
+class Crew {
+    constructor(name, position, staffNumber){
+        this.name = name
+        this.position = position
+        this.staffNumber = staffNumber
+    }
+
+}
+
+console.console.log(Crew);
